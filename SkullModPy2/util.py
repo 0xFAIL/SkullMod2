7ec3eb7cd88d0cf3174295b34e26e2967367035e
0xFAIL <0xFAIL@users.noreply.github.com>
2022-02-01 23:07:08 +0100

    Script should run on MacOS now, fixed vdf parsing

--- SkullModPy2/util.py
+++ SkullModPy2/util.py
@@ -1,11 +1,17 @@
 import math
 import os
 import struct
-import winreg
+import sys
+
+try:
+    import winreg
+except ImportError:
+    # Non-Windows platform, handled in methods
+    pass
 
 
 # Registry
-def get_reg_key(path, name):
+def get_reg_key_win(path, name):
     registry_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, path, 0, winreg.KEY_READ)
     value, regtype = winreg.QueryValueEx(registry_key, name)
     winreg.CloseKey(registry_key)
@@ -13,40 +19,45 @@ def get_reg_key(path, name):
 
 
 # Steam
-def get_steam_dir():
+def get_steam_dir_win():
     try:
         # Get 64-bit steam dir
-        return get_reg_key('SOFTWARE\\WOW6432Node\\Valve\\Steam', 'InstallPath')
-    except WindowsError:
+        return get_reg_key_win('SOFTWARE\\WOW6432Node\\Valve\\Steam', 'InstallPath')
+    except OSError:
         try:
             # Get 32-bit steam dir
-            return get_reg_key('SOFTWARE\\Valve\\Steam', 'InstallPath')
-        except WindowsError:
+            return get_reg_key_win('SOFTWARE\\Valve\\Steam', 'InstallPath')
+        except OSError:
             return None
 
 
 # Naive parsing of libraryfolders.vdf
-def parse_libraryfolders_vdf(steam_dir):
+def parse_libraryfolders_vdf_win(steam_dir):
     libraryfolders_path = os.path.join(steam_dir, 'steamapps', 'libraryfolders.vdf')
     library_dirs = [steam_dir]
 
     with open(libraryfolders_path, 'r') as lf_file:
         for line in lf_file.readlines():
-            if not line.lstrip().startswith('"'):
+            if not line.lstrip().startswith('"path"'):
                 continue
             # Get key and value
             segments = line.split('"')
-            if len(segments) != 5 or not segments[1].isdigit():
+            if len(segments) != 5:
                 continue
-            library_dirs.append(segments[3].replace('\\\\', '\\'))
+            new_steam_dir = segments[3].replace('\\\\', '\\')
+            if new_steam_dir not in library_dirs:
+                library_dirs.append(new_steam_dir)
     return library_dirs
 
 
 def get_data_directory():
-    steam_dir = get_steam_dir()
+    if sys.platform == 'darwin':  # MacOS
+        # Return guessed default location for MacOS, path is blind guess based on PR from GitHub
+        return '~/Library/Application Support/Steam/steamapps/common/Skullgirls/data01'
+    steam_dir = get_steam_dir_win()
     if steam_dir is None:
         return None
-    steam_libraries_list = parse_libraryfolders_vdf(steam_dir)
+    steam_libraries_list = parse_libraryfolders_vdf_win(steam_dir)
     # Try to find the steam library with the game
     for steam_library in steam_libraries_list:
         if os.path.exists(os.path.join(steam_library, 'steamapps', 'appmanifest_245170.acf')):
